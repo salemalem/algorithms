37e8b027069effded2b1ae548320cd0626648609
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-12-01 11:12:13 +0700

    pascal triangle for looping added

--- recursion/pascal_triangle.py
+++ recursion/pascal_triangle.py
@@ -1,10 +1,8 @@
-height = 4
+height = 5
 def pascal_number(row, column):
     if row <= 0 or column <= 0:
         return "I can't calculate it yet"
-    elif row == 1 or row == 2:
-        return 1
-    elif column == 1 or column == height:
+    elif row == 1 or row == 2 or column == 1 or column == row:
         return 1
     else:
         return pascal_number(row - 1, column - 1) + pascal_number(row - 1, column)
@@ -14,10 +12,10 @@ TO DO: Edit height properly before calling the function
 """
 
 # print(pascal_number(2, 2))
-print(pascal_number(4, 2))
+# print(pascal_number(4, 2))
 # print(pascal_number(100, 50))
-# for row in range(height + 1):
-#     for column in range(row + 1):
-#         print(pascal_number(row, column), end=" ")
-#     print()
+for row in range(1, height + 1):
+    for column in range(1, row + 1):
+        print(pascal_number(row, column), end=" ")
+    print()
     
