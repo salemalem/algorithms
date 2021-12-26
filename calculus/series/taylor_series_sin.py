356c96f101fc1dd4e84fd464d432651303d571c2
Shyngys Shynbolatov <shyngys.shynbolatov@gmail.com>
2021-12-26 23:39:51 +0700

    taylor series second loop; possible solution with if

--- calculus/series/taylor_series_sin.py
+++ calculus/series/taylor_series_sin.py
@@ -1,13 +1,18 @@
 from math import factorial , pi
 
 # x = pi
-x = pi / 3
+x = pi / 4
 # x = 10**-8
 
 approx_sum = x # starting sum
 
-for n in range(1, 51):
-  approx_sum -= ((-1) ** (n + 1)) * (x ** (2 * n + 1)) / (factorial(2 * n + 1))
+# for n in range(1, 51):
+#   approx_sum -= ((-1) ** (n + 1)) * (x ** (2 * n + 1)) / (factorial(2 * n + 1))
+
+for n in range(1, 101):
+  if n % 2 == 0:
+    approx_sum += ((-1) ** (n + 1)) * (x ** (n + 1)) / (factorial(n + 1))
+
 
 approx_sum = round(approx_sum, 5)
 print(approx_sum)
